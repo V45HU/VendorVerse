c3d77412ec3e6ba83487e98dc6f9083308b3653d
Vashu <vashunishadofficial45@gmail.com>
2026-06-23 23:27:30 +0530

    Created Portfolio.js model

---
+++ server/models/Portfolio.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const portfolioSchema = new mongoose.Schema(
+  {
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Portfolio = mongoose.model(
+  "Portfolio",
+  portfolioSchema
+);
+
+export default Portfolio;
